683362bd8c50cde34b9746b4f075d1c5e895811b
Gureboy <43415644+Gureboy@users.noreply.github.com>
2024-06-27 08:55:08 -0300

    Create Raro.cpp

---
+++ Raro.cpp
@@ -0,0 +1,14 @@
+String urlEncode(const String &str) {
+  String encodedString = "";
+  char c;
+  char code0;
+  char code1;
+  char code2;
+  for (int i = 0; i < str.length(); i++) {
+    c = str.charAt(i);
+    if (c == ' ') {
+      encodedString += '+';
+    } else if (isalnum(c)) {
+      encodedString += c;
+    } else {
+      code1 = (c & 0xf) + '
